3f99876f487ed7398a0529100be2cfd4780a1147
Chiel Kooijman <chiel999@gmail.com>
2012-05-30 22:15:51 +0200

    Updates description

--- src/dp.cpp
+++ src/dp.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <map>
-#include <list>
+#include <set>
 #include <algorithm>
 
 #define EE (0)
@@ -10,7 +10,7 @@
 
 
 /**
- * lists should be sorted and of the same length.
+ * multisets should be of the same length.
  *
  *	Return values:
  * Identical:	 0
@@ -20,12 +20,12 @@
  */
 
 	int
-list_compare(std::list<int> a, std::list<int> b)
+multiset_compare(std::multiset<int> a, std::multiset<int> b)
 {
 	int a_count = 0;
 	int b_count = 0;
-	std::list<int>::iterator ia;
-	std::list<int>::iterator ib;
+	std::multiset<int>::iterator ia;
+	std::multiset<int>::iterator ib;
 
 	for (ia=a.begin(), ib=b.begin(); ia!=a.end() && ib!=b.end(); ia++, ib++) {
 		if (*ia == *ib) {
@@ -52,60 +52,59 @@ list_compare(std::list<int> a, std::list<int> b)
 class Q
 {
 	public:
-		Q(std::list<int> v);
-		void set_default(std::list<int> v);
-		std::multimap<int, std::list<int> >::iterator operator[](const int key);
-		void insert(const int key, const std::list<int> value);
-		void add(const int key, std::list<int> value);
+		Q(std::multiset<int> v);
+		void multiset_default(std::multiset<int> v);
+		std::multimap<int, std::multiset<int> >::iterator operator[](const int key);
+		void insert(const int key, const std::multiset<int> value);
+		void add(const int key, std::multiset<int> value);
 	private:
-		std::multimap<int, std::list<int> > m;
-		std::list<int> default_v;
+		std::multimap<int, std::multiset<int> > m;
+		std::multiset<int> default_v;
 
 };
 
-Q::Q(std::list<int> v=NULL)
+Q::Q(std::multiset<int> v)
 {
 	default_v = v;
 }
 
 	void
-Q::set_default(std::list<int> v)
+Q::multiset_default(std::multiset<int> v)
 {
 	default_v = v;
 }
 
 	inline void
-Q::insert(const int key, const std::list<int> value)
+Q::insert(const int key, const std::multiset<int> value)
 {
-	m.insert(std::pair<int, std::list<int> >(key, value));
+	m.insert(std::pair<int, std::multiset<int> >(key, value));
 }
 
-	inline std::multimap<int, std::list<int> >::iterator
+	inline std::multimap<int, std::multiset<int> >::iterator
 Q::operator[](const int key)
 {
 	return m.find(key);
 }
 
 	void
-Q::add(const int key, std::list<int> value)
+Q::add(const int key, std::multiset<int> value)
 {
-	std::multimap<int, std::list<int> >::iterator it = m.find(key);
-	value.sort();
+	std::multimap<int, std::multiset<int> >::iterator it = m.find(key);
 	bool done = false;
 	for (; it != m.end() && !done; it++) {
-		switch (list_compare(value, it->second)) {
-			case 0: // same list already in key
+		switch (multiset_compare(value, it->second)) {
+			case 0: // same multiset already in key
 				return;
-			case 1: // new list is better; try to remove more before adding
+			case 1: // new multiset is better; try to remove more before adding
 				m.erase(it);
 				break;
-			case -1: // better list already in key
+			case -1: // better multiset already in key
 				return;
 			case 2: // pareto-equal: add later if not case 0 or 2
 				break;
 		}
 	}
-	insert(key, value); // no lists were better; add
+	insert(key, value); // no multisets were better; add
 	return;
 }
 
@@ -113,27 +112,33 @@ Q::add(const int key, std::list<int> value)
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	list<int> defval;
-	list<int> r1;
-	list<int> r2;
-	list<int> r3;
-	list<int> r4;
-	list<int> r5;
+	multiset<int> defval;
+	multiset<int> r1;
+	multiset<int> r2;
+	multiset<int> r3;
+	multiset<int> r4;
+	multiset<int> r5;
 
-	defval.push_back(0);
-	defval.push_back(0);
+	defval.insert(0);
+	defval.insert(0);
 
-	r1.push_back(1);
-	r1.push_back(1);
+	r1.insert(1);
+	r1.insert(1);
 
-	r2.push_back(10);
-	r2.push_back(2);
+	r2.insert(10);
+	r2.insert(2);
 
-	r3.push_back(3);
-	r3.push_back(3);
+	r3.insert(3);
+	r3.insert(3);
+	r3.insert(4);
+	r3.insert(1);
 
-	r4.push_back(10);
-	r4.push_back(10);
+	for (multiset<int>::iterator it = r3.begin(); it!=r3.end(); ++it) {
+		cout << *it << " ";
+	}
+
+	r4.insert(10);
+	r4.insert(10);
 
 	Q q = Q(defval);
 	q.add(EE, r1);
@@ -141,7 +146,7 @@ int main(int argc, char const *argv[])
 	q.add(EE, r3);
 	q.add(EE, r4);
 
-	for (list<int>::iterator it=q[EE]->second.begin(); it!=q[EE]->second.end(); ++it) {
+	for (multiset<int>::iterator it=q[EE]->second.begin(); it!=q[EE]->second.end(); ++it) {
 		cout << *it << " ";
 	}
 	cout << endl;
